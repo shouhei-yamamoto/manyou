4cf3ea2ca9785ec73b4add44b0144138dd073519
shouhei-yamamoto <elefish19920509@gmail.com>
2022-03-12 12:55:38 +0900

    ログイン機能の実装

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'kaminari'
 
 gem 'faker'
 
+gem 'bcrypt'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'tasks#index'
   resources :tasks
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
